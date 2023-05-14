c3434eda7663ede11477dea4fe21c05a3e59de03
David Mar <david.bryan.mar@gmail.com>
2023-05-14 17:32:24 -0500

    Update audio_server_start.py
    updated to have debug logs.

--- server/audio_server_start.py
+++ server/audio_server_start.py
@@ -1,7 +1,21 @@
+from datetime import datetime
 from flask import Flask, request
 import os
-from datetime import datetime
+
 app = Flask(__name__)
+
+import logging
+
+# Set up a file handler for the logger
+file_handler = logging.FileHandler('flask.log')
+file_handler.setLevel(logging.INFO)
+
+# Get the Flask app's logger
+app_logger = app.logger
+app_logger.addHandler(file_handler)
+app_logger.setLevel(logging.INFO)
+
+
 @app.route('/upload', methods=['POST'])
 def upload_file():
     if 'audio' not in request.files:
@@ -11,5 +25,20 @@ def upload_file():
     filename = datetime.now().strftime('%Y-%m-%d_%H-%M-%S.flac')
     file.save(os.path.join('./uploaded_audio_files/', filename))
     return 'File uploaded successfully', 200
+
+def create_dirs(directory_path):
+    print("in function: create_dirs")
+    if not os.path.isdir(directory_path):
+        app.logger.info("Creating dir {}".format(directory_path))
+        os.mkdir(directory_path)
+    else:
+        app.logger.info("Found existing dir {}".format(directory_path))
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.logger.info("Starting {}\n".format(datetime.now().strftime('%Y-%m-%d_%H-%M-%S')))
+    input_dir = "uploaded_audio_files"
+    output_dir = "output_audio_txt"
+    create_dirs(input_dir)
+    create_dirs(output_dir)
+
+    app.run(host='0.0.0.0', port=5000, debug=True)
