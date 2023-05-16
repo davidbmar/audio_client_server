4d3db6d5316e5abdfafc25a0a94ae72b481e2348
David Mar <david.bryan.mar@gmail.com>
2023-05-15 22:56:50 -0500

    Create receive_text.py
    Initial commit

---
+++ client/receive_text.py
@@ -0,0 +1,13 @@
+# This is the "server" side, where the client is recieving voice to txt data from the Whisper server. 
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    file_contents = request.json['file_contents']
+    # do something with file_contents...
+    return 'Files received'
+
+if __name__ == '__main__':
+    app.run(port=5000)
