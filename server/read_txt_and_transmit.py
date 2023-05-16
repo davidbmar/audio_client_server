a6396bcc35e9a9f0eb7bb62b5b41dcf639fabe27
davidbmar <david.bryan.mar@gmail.com>
2023-05-16 04:32:11 +0000

    updated to use a path for the upload dir.

--- server/read_txt_and_transmit.py
+++ server/read_txt_and_transmit.py
@@ -2,6 +2,7 @@
 #Note that the original definition the place where this code resides was on the server and it was connected to the client.
 #But this is in reverse, as now the server has the data and sends this info to the server.
 import requests
+import glob
 import os
 
 def read_files(filenames):
@@ -10,6 +11,24 @@ def read_files(filenames):
         with open(fn, 'r') as f:
             contents[fn] = f.read()
     return contents
+
+
+# specify the directory you want to scan
+# for now its just going to be uploaded_audio_files.  
+# TODO: Later you can change this to the output dir.
+directory_path = './uploaded_audio_files/'
+
+# get a list of all files in the directory
+files = glob.glob(directory_path + '*')
+
+# iterate over the list of files
+for file in files:
+    print("file {}".format(file))
+    # open each file in read mode
+    with open(file, 'r') as f:
+        # do something with the file content
+        content = f.read()
+        print(content)
 
 files_to_send = ["file1.txt", "file2.txt", "file3.txt"]  # replace with your actual file paths
 file_contents = read_files(files_to_send)
