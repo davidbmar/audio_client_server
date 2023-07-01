3efcf619fb026107890c82ffbf021ea3007ad37d
David Mar <david.bryan.mar@gmail.com>
2023-07-01 17:36:20 +0000

    added helper bash for running on EC2 linux Ubuntu.

---
+++ server/setup_server_ec2_ubuntu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#If running on EC2 ubuntu run this script to help set things up.
+sudo apt-get update
+sudo apt-get install python3-venv
+sudo pip3 install flask
+sudo apt-get install gh
