81a9989c812181dc15c1e31fa41f9a8c2b46097e
David Mar <david.bryan.mar@gmail.com>
2023-07-27 21:34:36 +0000

    updates for running on EC2.

---
+++ server/lambda_prototype.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+import boto3
+import requests
+import json
+
+# Create a session using your AWS credentials
+sqs = boto3.client('sqs', region_name='us-west-2')  # replace with your region
+
+queue_url = 'https://sqs.us-west-2.amazonaws.com/123456789012/myqueue'  # replace with your queue URL
+
+def process_messages():
+    while True:
+        response = sqs.receive_message(
+            QueueUrl=queue_url,
+            AttributeNames=['All'],
+            MaxNumberOfMessages=1,
+            VisibilityTimeout=0,
+            WaitTimeSeconds=0
+        )
+
+        if 'Messages' in response:  # when the queue is exhausted, the response dict contains no 'Messages' key
+            for message in response['Messages']:  # iterate over messages
+                # print the message body
+                event_data = json.loads(message['Body'])
+
+                # send to flask server
+                url = 'http://myflaskserver.com:5000/process_s3_object'  # replace with your flask server url
+                headers = {'Content-Type': 'application/json'}
+                server_response = requests.post(url, data=json.dumps(event_data), headers=headers)
+
+                if server_response.status_code == 204:
+                    print('Successfully sent data to Flask server.')
+                else:
+                    print('Failed to send data. Flask server responded with status:', server_response.status_code)
+
+                # delete the message from the queue to prevent it from being processed again
+                sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=message['ReceiptHandle'])
+
+        else:
+            print('No messages in queue. Sleeping for 1 second...')
+            time.sleep(1)  # sleep for a while before checking the queue again
+
+if __name__ == '__main__':
+    process_messages()
+
