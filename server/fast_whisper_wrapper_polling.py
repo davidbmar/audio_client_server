f0193b7358aa1e28f31c9a598f046219cd64a704
David Mar <david.bryan.mar@gmail.com>
2023-08-21 02:08:12 +0000

    Fixed problem where there was a sync timing problem where it was uploading 0 length files.

--- server/fast_whisper_wrapper_polling.py
+++ server/fast_whisper_wrapper_polling.py
@@ -22,14 +22,23 @@ class SpeechTranscriber:
             raise SystemExit("previously_seen.txt file created, restart the program.")
 
     def transcribe(self, file):
-        """Transcribes a given .flac file and writes the result to the log file."""
-        segments, info = self.model.transcribe(file, language="en", beam_size=5)
-        with open(file+".jxt", 'a') as f:
-            for segment in segments:
-                transcription = segment.text.replace('\n', '') + '\n'
-                print("file:"+file)
-                print(transcription, end='')  # Print to console
-                f.write(transcription)  # Write to log file
+       """Transcribes a given .flac file and writes the result to a temporary file, then renames it to the final .txt file."""
+       segments, info = self.model.transcribe(file, language="en", beam_size=5)
+    
+       # Define the temporary and final file names
+       temp_file_name = file + ".tmp"
+       final_file_name = file + ".txt"
+
+       # Write to the temporary file
+       with open(temp_file_name, 'a') as f:
+           for segment in segments:
+               transcription = segment.text.replace('\n', '') + '\n'
+               print("file:" + file)
+               print(transcription, end='')  # Print to console
+               f.write(transcription)  # Write to temporary file
+   
+       # Rename the temporary file to the final file name
+       os.rename(temp_file_name, final_file_name)
 
     def transcribe(self, file):
        """Transcribes a given .flac file and writes the result to a temporary file, then renames it to the final .txt file."""
@@ -51,7 +60,6 @@ class SpeechTranscriber:
 
        os.rename(temp_file_name, final_file_name)
 
-
     def process_file(self, filename):
         """Checks if the file is a new .flac file and if so, transcribes it."""
         full_path = os.path.join(self.observed_dir, filename)
@@ -61,7 +69,6 @@ class SpeechTranscriber:
                     self.transcribe(full_path)
                     with open(self.seen_file, 'a') as seen_file:
                         seen_file.write(full_path + '\n')
- 
 
     def start(self):
         """Main loop that continuously polls the directory for new .flac files."""
---
+++ server/run_these.md
@@ -0,0 +1,6 @@
+run:
+1.fast_whisper_wrapper_polling.py
+2.s3_pull.py
+3.lambda_prototype.py
+4.s3_put.py
+
