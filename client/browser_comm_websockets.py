a6436f652124aa8aa811b7aa9902d99ad5908430
David Mar <david.bryan.mar@gmail.com>
2023-09-01 01:34:50 +0000

    This is the working code which on the server side it grabs the contents of the S3 object.

--- client/browser_comm_websockets.py
+++ client/browser_comm_websockets.py
@@ -5,6 +5,10 @@ import json
 import boto3
 import logging
 
+def fetch_s3_object_content(bucket_name, object_key):
+    s3_object = s3.get_object(Bucket=bucket_name, Key=object_key)
+    return s3_object['Body'].read().decode('utf-8')
+
 async def handler(websocket, path):
     try:
         while True:
@@ -16,12 +20,15 @@ async def handler(websocket, path):
             )
             messages = response.get('Messages', [])
             if messages:
+
                 for message in messages:
                     message_body = message['Body']
-                    logging.info(f"Fetched {len(messages)} messages: {message_body}")
-
-                    await websocket.send(json.dumps({'new_file': message_body}))
-
+        
+                    # Fetch content from S3
+                    s3_content = fetch_s3_object_content('audioclientserver-transcribedobjects-public', message_body)
+                    
+                    await websocket.send(json.dumps({'new_file_content': s3_content}))
+        
                     # Delete message from queue after sending
                     sqs.delete_message(
                         QueueUrl=queue_url,
@@ -32,11 +39,11 @@ async def handler(websocket, path):
 
             await asyncio.sleep(1)  # Sleep for 1 second before fetching more messages
 
+    except Exception as e:  # Added a general exception handler
+        logging.error(f"An error occurred: {str(e)}")
+
     except websockets.ConnectionClosed:
         logging.warning("Connection closed.")
-
-
-
 
 # Configure logging to write to a file
 logging.basicConfig(level=logging.INFO)
@@ -46,11 +53,14 @@ logging.basicConfig(level=logging.INFO)
 sqs = boto3.client('sqs', region_name='us-east-2')
 queue_url = 'https://sqs.us-east-2.amazonaws.com/635071011057/audio_client_server-browser_comm_websockets-sqs_queue.fifo'
 
-# Create a WebSocket server listening on all network interfaces (0.0.0.0) on port 8765
-start_server = websockets.serve(handler, "0.0.0.0", 8765)
-        
-# Start the server
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+try:  # Added a try-except block around server initialization
+    # Create a WebSocket server listening on all network interfaces (0.0.0.0) on port 8765
+    start_server = websockets.serve(handler, "0.0.0.0", 8766)
 
+    # Start the server
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
+
+except Exception as e:
+    logging.error(f"Failed to start server: {str(e)}")
 
