9e0e60dba0565e5299a7c1c3542627c7d88ad15e
David Mar <david.bryan.mar@gmail.com>
2023-09-06 04:24:42 +0000

    updates to the client to show files that are uploaded.

--- client/browser_comm_websockets.py
+++ client/browser_comm_websockets.py
@@ -6,10 +6,27 @@ import boto3
 import logging
 
 def fetch_s3_object_content(bucket_name, object_key):
+    """
+    Fetch the content of an S3 object given a bucket name and object key.
+    
+    Parameters:
+    bucket_name (str): The name of the S3 bucket.
+    object_key (str): The key of the object within the S3 bucket.
+    
+    Returns:
+    str: The content of the S3 object decoded as a string.
+    """
     s3_object = s3.get_object(Bucket=bucket_name, Key=object_key)
     return s3_object['Body'].read().decode('utf-8')
 
 async def handler(websocket, path):
+    """
+    Async handler for handling WebSocket connections.
+    
+    Parameters:
+    websocket (websockets.WebSocketServerProtocol): The WebSocket server protocol.
+    path (str): The path of the request URI.
+    """
     try:
         while True:
             # Fetch messages from SQS
@@ -18,37 +35,36 @@ async def handler(websocket, path):
                 MaxNumberOfMessages=1,
                 VisibilityTimeout=5  # 5 seconds
             )
+            logging.info(f"SQS Response: {response}")  # Log the SQS response
             messages = response.get('Messages', [])
+            
+            # Check if there are messages to process
             if messages:
-
                 for message in messages:
                     message_body = message['Body']
-        
-                    # Fetch content from S3
-                    s3_content = fetch_s3_object_content('audioclientserver-transcribedobjects-public', message_body)
                     
+                    # Fetch content from S3 and send it over the WebSocket
+                    logging.info(f"Fetching content from S3 for message: {message_body}")  # Log before fetching from S3
+                    s3_content = fetch_s3_object_content('audioclientserver-transcribedobjects-public', message_body)
                     await websocket.send(json.dumps({'new_file_content': s3_content}))
-        
-                    # Delete message from queue after sending
+                    
+                    # Delete message from SQS queue after sending
                     sqs.delete_message(
                         QueueUrl=queue_url,
                         ReceiptHandle=message['ReceiptHandle']
                     )
             else:
                 logging.info("No messages to fetch.")
-
+            
             await asyncio.sleep(1)  # Sleep for 1 second before fetching more messages
-
-    except Exception as e:  # Added a general exception handler
+            
+    except Exception as e:
         logging.error(f"An error occurred: {str(e)}")
-
     except websockets.ConnectionClosed:
         logging.warning("Connection closed.")
 
-# Configure logging to write to a file
+# Configure logging
 logging.basicConfig(level=logging.INFO)
-#logging.basicConfig(filename='browser_comm_websockets.server.log', level=logging.INFO)
-
 
 # Initialize SQS and S3 clients
 sqs = boto3.client('sqs', region_name='us-east-2')
@@ -56,14 +72,13 @@ s3 = boto3.client('s3', region_name='us-east-2')
 
 queue_url = 'https://sqs.us-east-2.amazonaws.com/635071011057/audio_client_server-browser_comm_websockets-sqs_queue.fifo'
 
-try:  # Added a try-except block around server initialization
-    # Create a WebSocket server listening on all network interfaces (0.0.0.0) on port 8765
+try:
+    # Start WebSocket server
     start_server = websockets.serve(handler, "0.0.0.0", 8766)
-
-    # Start the server
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
-
+    
 except Exception as e:
     logging.error(f"Failed to start server: {str(e)}")
+
 
--- client/cmd_line_test-browser_comm_websockets.py
+++ client/cmd_line_test-browser_comm_websockets.py
@@ -37,7 +37,12 @@ def send_batch_messages(message_list):
 
 if __name__ == '__main__':
     # Add the first 5 files at once
-    first_files = [ "audio-1693524330757.flac.txt", "audio-1693524340758.flac.txt", "audio-1693524350758.flac.txt"]
+    first_files = [ "2023-09-04-000100.flac.txt",
+            "2023-09-04-000101.flac.txt",
+            "2023-09-04-000102.flac.txt",
+            "2023-09-04-000103.flac.txt",
+            "2023-09-04-000104.flac.txt",
+            "2023-09-04-000105.flac.txt"]
 
     send_batch_messages(first_files)
 
@@ -45,7 +50,17 @@ if __name__ == '__main__':
     time.sleep(1)
 
     # Add the next 3 files one at a time
-    next_files = [ "audio-1693524360758.flac.txt", "audio-1693524370757.flac.txt", "audio-1693524408816.flac.txt"]
+    next_files = ['2023-09-04-000106.flac.txt',
+            '2023-09-04-000107.flac.txt',
+            '2023-09-04-000108.flac.txt',
+            '2023-09-04-000109.flac.txt',
+            '2023-09-04-000110.flac.txt',
+            '2023-09-04-000111.flac.txt',
+            '2023-09-04-000112.flac.txt',
+            '2023-09-04-000113.flac.txt',
+            '2023-09-04-000114.flac.txt']
+
+    send_batch_messages(first_files)
     for f in next_files:
         send_message(f)
         time.sleep(1)  # Wait for 5 seconds before sending the next file
