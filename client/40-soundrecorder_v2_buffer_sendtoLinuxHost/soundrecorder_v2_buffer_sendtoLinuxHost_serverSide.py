bc50e03e6728a0db3977b7fe6c2e1c77c583843c
David Mar <david.bryan.mar@gmail.com>
2023-09-16 03:45:44 +0000

    modified soundrecorder to input into the fast_whisper_wrappersqsqueue.

--- client/40-soundrecorder_v2_buffer_sendtoLinuxHost/soundrecorder_v2_buffer_sendtoLinuxHost_serverSide.py
+++ client/40-soundrecorder_v2_buffer_sendtoLinuxHost/soundrecorder_v2_buffer_sendtoLinuxHost_serverSide.py
@@ -7,7 +7,9 @@ import os
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
-UPLOAD_FOLDER = './uploads'
+
+UPLOAD_FOLDER = '../../server/s3-downloads'
+#UPLOAD_FOLDER = './uploads'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
@@ -24,6 +26,18 @@ def upload_file():
         filepath = os.path.join(UPLOAD_FOLDER, filename)
         file.save(filepath)
         return f'File {filename} uploaded successfully', 200
+
+        file_ready(filename)
+
+def file_ready(file_info):
+    sqs = boto3.client('sqs',region_name='us-east-2')
+    
+    queue_url = 'https://sqs.us-east-2.amazonaws.com/635071011057/fast_whisper_wrapper_sqs_queue.fifo'
+    
+    # Send the file information to the queue
+    message_group_id="user1" # for now 1, as there is no user id. #todo change later.
+    sqs.send_message(QueueUrl=queue_url, MessageBody=file_info, MessageGroupId=message_group_id)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8768)
