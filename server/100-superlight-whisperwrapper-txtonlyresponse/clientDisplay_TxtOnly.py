4c93de42c89f574e8a461c252642240927d8c604
David Mar <david.bryan.mar@gmail.com>
2023-09-17 04:34:47 +0000

    updated to the correct sqs queuegit add clientDisplay_TxtOnly.py !

--- server/100-superlight-whisperwrapper-txtonlyresponse/clientDisplay_TxtOnly.py
+++ server/100-superlight-whisperwrapper-txtonlyresponse/clientDisplay_TxtOnly.py
@@ -3,34 +3,47 @@ import asyncio
 import websockets
 import boto3
 import json
+import time  # Importing the time module
 
 class WebSocketSQSServer:
     def __init__(self, sqs_queue_url, ws_port=8766):
+        print("Initializing WebSocketSQSServer...")  # Debugging statement
         self.sqs = boto3.client('sqs', region_name='us-east-2')
         self.sqs_queue_url = sqs_queue_url
         self.ws_port = ws_port
 
     async def producer_handler(self, websocket, path):
         try:
+            print("Entered producer_handler...")  # Debugging statement
             ping_interval = 10  # Ping every 10 seconds
             last_ping_time = time.time()
-            
+
             while True:
                 current_time = time.time()
-                
+
                 if current_time - last_ping_time >= ping_interval:
+                    print("Sending ping...")  # Debugging statement
                     await websocket.ping("keepalive")
                     last_ping_time = current_time
-                
+
+                print("Fetching messages from SQS...")  # Debugging statement
                 messages = self.sqs.receive_message(QueueUrl=self.sqs_queue_url, MaxNumberOfMessages=1)
+                
                 if 'Messages' in messages:
+                    print(f"Received messages: {messages}")  # Debugging statement
                     for message in messages['Messages']:
                         message_content = message['Body']
+                        print(f"Sending message: {message_content}")  # Debugging statement
+                        json_payload = json.dumps({'file_info': message_content})
+                        print(f"JSON payload: {json_payload}")  # <-- Add this line
+
                         await websocket.send(json.dumps({'file_info': message_content}))
                         self.sqs.delete_message(QueueUrl=self.sqs_queue_url, ReceiptHandle=message['ReceiptHandle'])
-                
+                else:
+                    print("No messages in SQS.")  # Debugging statement
+
                 await asyncio.sleep(1)
-                
+
         except websockets.exceptions.ConnectionClosedOK:
             print("Connection closed, exiting.")
         except Exception as e:
@@ -41,11 +54,12 @@ class WebSocketSQSServer:
         asyncio.set_event_loop(loop)
         server = websockets.serve(self.producer_handler, "0.0.0.0", self.ws_port)
         loop.run_until_complete(server)
-        print(f"WebSocket server started on ws://0.0.0.0:{self.ws_port}")
+        print(f"WebSocket server started on ws://0.0.0.0:{self.ws_port}")  # Debugging statement
         loop.run_forever()
 
 if __name__ == "__main__":
-    sqs_queue_url = 'https://sqs.us-east-2.amazonaws.com/635071011057/transcribe_NoS3.fifo'
+    print("Starting WebSocketSQSServer...")  # Debugging statement
+    sqs_queue_url = 'https://sqs.us-east-2.amazonaws.com/635071011057/clientDisplay_TxtOnly.fifo'
     ws_server = WebSocketSQSServer(sqs_queue_url)
     ws_server.start_server()
 
