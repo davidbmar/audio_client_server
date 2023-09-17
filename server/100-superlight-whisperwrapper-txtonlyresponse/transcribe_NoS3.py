400d549f6f38f2f3aab367052616ea0f6cc540ca
David Mar <david.bryan.mar@gmail.com>
2023-09-17 00:10:07 +0000

    This now is the files to provide superlightweight fast transcription.

--- server/100-superlight-whisperwrapper-txtonlyresponse/transcribe_NoS3.py
+++ server/100-superlight-whisperwrapper-txtonlyresponse/transcribe_NoS3.py
@@ -8,7 +8,7 @@ from faster_whisper import WhisperModel
 
 class SpeechTranscriber:
     def __init__(self, model_size="large-v2", device="cuda", compute_type="float16",
-                 download_dir="./s3-downloads", log_file="transcriptions.txt"):
+                 download_dir="./recievedSoundFiles", log_file="transcriptions.txt"):
         self.model = WhisperModel(model_size, device=device, compute_type=compute_type)
         self.download_dir = download_dir
         self.log_file = log_file
@@ -16,35 +16,36 @@ class SpeechTranscriber:
         self.transcription_queue_url = 'https://sqs.us-east-2.amazonaws.com/635071011057/transcribe_NoS3.fifo'
         self.final_file_txt_file_queue_url = 'https://sqs.us-east-2.amazonaws.com/635071011057/clientDisplay_TxtOnly.fifo'
 
-    def send_to_final_file_queue(self, filename):
-        """Send filename to the new SQS queue."""
+    # Modified Code
+    def send_to_final_file_queue(self, filename, transcribed_message):
+        """Send filename and transcribed_message to the new SQS queue."""
+        message_body = {
+            'filename': filename,
+            'transcribed_message': transcribed_message
+        }
         response = self.sqs.send_message(
             QueueUrl=self.final_file_txt_file_queue_url,
-            MessageBody=filename,
+            MessageBody=str(message_body),
+            MessageDeduplicationId='MessageGroupIDUserA',
             MessageGroupId='MessageGroupIDUserA'
         )
-        print(f"Sent {filename} to new SQS queue for final files")
+        print(f"Sent {filename} and its transcription to new SQS queue for final files")
 
+    # Modified Code
     def transcribe(self, file):
         segments, info = self.model.transcribe(file, language="en", beam_size=5)
-
-        temp_file_name = file + ".tmp"
-        final_file_name = file + ".txt"
-
-        with open(temp_file_name, 'a') as f:
-            for segment in segments:
-                transcription = segment.text.replace('\n', '') + '\n'
-                print("file:" + file)
-                print(transcription, end='')
-                f.write(transcription)
-
-        os.rename(temp_file_name, final_file_name)
-
-        # Send the final file name to the new SQS queue
-        # Extract only the base filename and send it to the queue.
-        final_file_base_name = os.path.basename(final_file_name)
-        self.send_to_final_file_queue(final_file_base_name)
-
+        transcribed_message = ""
+    
+        for segment in segments:
+            transcription = segment.text.replace('\n', '') + '\n'
+            transcribed_message += transcription  # Collecting all transcribed segments
+            print("file:" + file)
+            print(transcription, end='')
+    
+        # Send the final file name and transcribed message to the new SQS queue
+        final_file_base_name = os.path.basename(file)  # Extracting base filename from the full path
+        self.send_to_final_file_queue(final_file_base_name, transcribed_message)  # Modified line
+    
     def process_file(self, filename):
         full_path = os.path.join(self.download_dir, filename)
 
---
+++ server/100-superlight-whisperwrapper-txtonlyresponse/utility.pop.clientDisplay_TxtOnly.fifo.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import boto3
+from pprint import pprint
+
+# Initialize the boto3 client for SQS
+sqs = boto3.client('sqs', region_name='us-east-2')
+
+# The URL of the SQS queue
+queue_url = 'https://sqs.us-east-2.amazonaws.com/635071011057/clientDisplay_TxtOnly.fifo'
+
+def receive_message_from_queue():
+    response = sqs.receive_message(
+        QueueUrl=queue_url,
+        MaxNumberOfMessages=1,  # Only fetch one message
+        WaitTimeSeconds=0,  # Don't wait for messages
+        AttributeNames=['All']  # Fetch all attributes
+    )
+
+    # Check if a message was received
+    if 'Messages' in response:
+        message = response['Messages'][0]
+        receipt_handle = message['ReceiptHandle']
+
+        # Pretty-print the body and all attributes of the message
+        print("Received message:")
+        pprint(message['Body'])
+        print("Message Attributes:")
+        pprint(message.get('Attributes', {}))
+
+
+
+# Execute the function
+if __name__ == "__main__":
+    receive_message_from_queue()
+
