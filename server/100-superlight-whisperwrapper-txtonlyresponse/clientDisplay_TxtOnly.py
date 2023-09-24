c9c2435f1c53e91284ec51cf0174c4c3aa0635bd
David Mar <david.bryan.mar@gmail.com>
2023-09-24 05:52:16 +0000

    fixed the heartbeat issues.

--- server/100-superlight-whisperwrapper-txtonlyresponse/clientDisplay_TxtOnly.py
+++ server/100-superlight-whisperwrapper-txtonlyresponse/clientDisplay_TxtOnly.py
@@ -7,7 +7,7 @@ import time  # Importing the time module
 
 class WebSocketSQSServer:
     def __init__(self, sqs_queue_url, ws_port=8766):
-        print("Initializing WebSocketSQSServer...")  # Debugging statement
+        #print("Initializing WebSocketSQSServer...")  # Debugging statement
         self.sqs = boto3.client('sqs', region_name='us-east-2')
         self.sqs_queue_url = sqs_queue_url
         self.ws_port = ws_port
@@ -26,47 +26,63 @@ class WebSocketSQSServer:
 
     async def producer_handler(self, websocket, path):
         try:
-            print("Entered producer_handler...")  # Debugging statement
-            ping_interval = 10  # Ping every 10 seconds
-            last_ping_time = time.time()
-            websocket.pong_received = self.pong_received  # set the pong handler
-
-
             while True:
-                current_time = time.time()
-
-                if current_time - last_ping_time >= ping_interval:
-                    if not self.awaiting_pong:  # <-- New Line: check if a pong is pending
-                        print("Sending ping...")  # Debugging statement
-                        await websocket.ping("keepalive")
-                        self.awaiting_pong = True  # <-- New Line: set the flag
-                        last_ping_time = current_time
-                    else:
-                        print("Skipped sending ping because waiting for pong.")
-
-
-                print("Fetching messages from SQS...")  # Debugging statement
-                messages = self.sqs.receive_message(QueueUrl=self.sqs_queue_url, MaxNumberOfMessages=10)
-               
+                messages = self.sqs.receive_message(QueueUrl=self.sqs_queue_url, MaxNumberOfMessages=5, WaitTimeSeconds=10)
                 if 'Messages' in messages:
-                    print(f"Fetched {len(messages['Messages'])} messages.")  # Debugging statement
                     for message in messages['Messages']:
                         message_content = message['Body']
-                        print(f"message_content: {message_content}")  # Debugging statement
-                        json_payload = json.dumps({'file_info': message_content})
-                        print(f"JSON payload: {json_payload}")  # <-- Add this line
+                        print(f"Received Message: {message_content}")
 
                         await websocket.send(json.dumps({'file_info': message_content}))
                         self.sqs.delete_message(QueueUrl=self.sqs_queue_url, ReceiptHandle=message['ReceiptHandle'])
                 else:
-                    print("No messages in SQS.")  # Debugging statement
-
-                await asyncio.sleep(1)
-
-        except websockets.exceptions.ConnectionClosedOK:
-            print("Connection closed, exiting.")
+                    print("No messages received.")
+                await asyncio.sleep(0.1)
         except Exception as e:
             print(f"An error occurred: {e}")
+    
+
+
+#    async def producer_handler(self, websocket, path):
+#        try:
+#            #print("Entered producer_handler...")  # Debugging statement
+#            ping_interval = 10  # Ping every 10 seconds
+#            last_ping_time = time.time()
+#            websocket.pong_received = self.pong_received  # set the pong handler
+#
+#
+#            while True:
+#                current_time = time.time()
+#
+#                if current_time - last_ping_time >= ping_interval:
+#                    if not self.awaiting_pong:  # <-- New Line: check if a pong is pending
+#                        #print("Sending ping...")  # Debugging statement
+#                        await websocket.ping("keepalive")
+#                        self.awaiting_pong = True  # <-- New Line: set the flag
+#                        last_ping_time = current_time
+#                    else:
+#                        print("Skipped sending ping because waiting for pong.")
+#                #messages = self.sqs.receive_message(QueueUrl=self.sqs_queue_url, MaxNumberOfMessages=10)
+#                messages = self.sqs.receive_message(QueueUrl=self.sqs_queue_url, MaxNumberOfMessages=5, WaitTimeSeconds=10)
+#
+#               
+#                if 'Messages' in messages:
+#                    for message in messages['Messages']:
+#                        message_content = message['Body']
+#                        json_payload = json.dumps({'file_info': message_content})
+#                        print(f"JSON payload: {json_payload}")  # <-- Add this line
+#
+#                        await websocket.send(json.dumps({'file_info': message_content}))
+#                        self.sqs.delete_message(QueueUrl=self.sqs_queue_url, ReceiptHandle=message['ReceiptHandle'])
+#                else:
+#                    print(".")  # Debugging statement
+#
+#                await asyncio.sleep(0.1)
+#
+#        except websockets.exceptions.ConnectionClosedOK:
+#            print("Connection closed, exiting.")
+#        except Exception as e:
+#            print(f"An error occurred: {e}")
 
     def start_server(self):
         loop = asyncio.new_event_loop()
