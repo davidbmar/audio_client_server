8a602c6dd1bea71618e6623e3f3b04a585fccd52
David Mar <david.bryan.mar@gmail.com>
2023-11-01 04:15:31 +0000

    updated this file to run with the commandline flag --run-once which really runs until there is nothing in the queue, but doesnt poll and loop.

--- server/300-write_to_s3_path/pull_transcribed_txt_from_sqs_queue.py
+++ server/300-write_to_s3_path/pull_transcribed_txt_from_sqs_queue.py
@@ -3,6 +3,11 @@ import boto3
 import csv
 import re
 import time
+import argparse
+
+parser = argparse.ArgumentParser(description="Run the script with a flag.")
+parser.add_argument("--run-once", action="store_true", help="If set, run the script once and exit.")
+args = parser.parse_args()
 
 def extract_key(filename):
     match = re.search(r'(\d{6})\.\w+$', filename)
@@ -33,20 +38,33 @@ def retrieve_messages_from_sqs(queue_url, num_messages=10):
     )
 
     messages = response.get('Messages', [])
+    receipt_handles = []  # List to store receipt handles for batch delete
     for message in messages:
         message_content = eval(message['Body'])
         update_csv_with_messages([message_content])
+        receipt_handles.append(message['ReceiptHandle'])
 
-        sqs_client.delete_message(
-            QueueUrl=queue_url,
-            ReceiptHandle=message['ReceiptHandle']
-        )
+    # Batch delete processed messages
+    if receipt_handles:
+        entries = [{'Id': str(i), 'ReceiptHandle': rh} for i, rh in enumerate(receipt_handles)]
+        sqs_client.delete_message_batch(QueueUrl=queue_url, Entries=entries)
 
     return messages
 
-# Continuously poll SQS queue and update CSV
-queue_url = 'https://sqs.us-east-2.amazonaws.com/635071011057/sqs_queue_runpodio_whisperprocessor_us_east_2_completed_transcription_nonfifo'
-while True:
-    retrieve_messages_from_sqs(queue_url)
-    time.sleep(2)
+
+def main():
+   # Continuously poll SQS queue and update CSV
+   queue_url = 'https://sqs.us-east-2.amazonaws.com/635071011057/sqs_queue_runpodio_whisperprocessor_us_east_2_completed_transcription_nonfifo'
+
+   if args.run_once:
+      print("Retrieveing while messages exist then exiting as opposed to forever polling..")
+      while retrieve_messages_from_sqs(queue_url):  # Continue until no messages are returned
+        pass
+   else:
+      while True:
+         retrieve_messages_from_sqs(queue_url)
+         time.sleep(2)
+
+if __name__ == "__main__":
+    main()
 
