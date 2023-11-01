aca519e4596fda27f447704a95bce1170431557d
David Mar <david.bryan.mar@gmail.com>
2023-11-01 04:22:32 +0000

    added test mock.

--- server/300-write_to_s3_path/utility.test.clearsqs.run.mocks.sh
+++ server/300-write_to_s3_path/utility.test.clearsqs.run.mocks.sh
@@ -16,17 +16,24 @@ sleep 2
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
 echo "Simulating a mock Runpod.io text transcriber"
 ./utility.mock_transcribe.py
+echo "DONE: Output should be in SQS Queue."
+echo "sqs_queue_runpodio_whisperprocessor_us_east_2_completed_transcription_nonfifo"
 sleep 2
 
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
 echo "./pull_transcribed_txt_from_sqs_queue.py --run-once"
 ./pull_transcribed_txt_from_sqs_queue.py --run-once
+echo "DONE: Pulled from the SQS Queue. Output should be in output.csv."
 sleep 2
 
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
 echo "./sort_csv.py"
 ./sort_csv.py
+echo "DONE: Sorted. Output should be in output.csv.sorted"
+sleep 2
 
-
+echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
+echo "./llm_transcription_summary.py."
+./llm_transcription_summary.py
 
 
