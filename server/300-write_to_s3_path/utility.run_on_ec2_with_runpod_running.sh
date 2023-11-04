7ce394d0d57ecbb039b376e110662ad2fddd4fbc
David Mar <david.bryan.mar@gmail.com>
2023-11-04 02:53:22 +0000

    fixed csv_to_html to properly render html files.  added utilities for testing.

--- server/300-write_to_s3_path/utility.run_on_ec2_with_runpod_running.sh
+++ server/300-write_to_s3_path/utility.run_on_ec2_with_runpod_running.sh
@@ -1,25 +1,4 @@
 #!/usr/bin/bash
-# clear the sqs queues
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
-echo "Clearing all the SQS Queues.\n\n"
-rm /home/ubuntu/audio_client_server/utilities/temp.output.txt
-/home/ubuntu/audio_client_server/utilities/clear_sqs_queues.py > temp.output.txt
-cat temp.output.txt; rm temp.output.txt
-
-sleep 4
-
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
-echo "Clearing the outputfile, otherwise it will concatenate it."
-rm output.csv
-sleep 2
-
-echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
-echo "Simulating a mock Runpod.io text transcriber --slowly-trickle-data 5 "
-./utility.mock_transcribe.py -slowly-trickle-data 5 &
-echo "DONE: Output should be in SQS Queue."
-echo "sqs_queue_runpodio_whisperprocessor_us_east_2_completed_transcription_nonfifo"
-sleep 2
-
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
 #echo "./pull_transcribed_txt_from_sqs_queue.py --run-once"
 echo "./pull_transcribed_txt_from_sqs_queue.py --loop-every-x-seconds 2"
@@ -44,6 +23,7 @@ rm csv_to_html_last_line.dat
 echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
 echo "./csv_to_html.py -loop-every-x-seconds 1 &"
 ./csv_to_html.py -loop-every-x-seconds 1 -last-line-processed 0 &
+
 #echo "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n\n"
 #echo "./llm_transcription_summary.py."
 #./llm_transcription_summary.py
