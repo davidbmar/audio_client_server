a0d292dcfa82f0ddbc399c720e2b93345f414d86
David Mar <david.bryan.mar@gmail.com>
2023-12-10 17:49:30 -0600

    This provides the framework which generates a webpage using S3 data from the .txt (transcribed files) and the audio files (.flac).  This prevents haveing to have a service because it can be pulled from a webpage.

---
+++ client/60-S3-audio_and_transcribed_files/s3_txt_and_audio.py
@@ -0,0 +1,113 @@
+#!/usr/bin/python3
+import boto3
+
+def list_files(bucket_name, extension, start_time, end_time):
+    """List files with a specific extension in the specified S3 bucket within the given time range, handling pagination."""
+    s3 = boto3.client('s3')
+    filtered_files = []
+    paginator = s3.get_paginator('list_objects_v2')
+
+    # Iterate over each page of results
+    for page in paginator.paginate(Bucket=bucket_name):
+        if 'Contents' in page:
+            for item in page['Contents']:
+                file_key = item['Key']
+                if file_key.endswith(extension) and start_time <= file_key <= end_time:
+                    filtered_files.append(file_key)
+    return filtered_files
+
+def get_file_content(bucket_name, file_key):
+    """Retrieve the content of a file from an S3 bucket."""
+    s3 = boto3.client('s3')
+    try:
+        obj = s3.get_object(Bucket=bucket_name, Key=file_key)
+        return obj['Body'].read().decode('utf-8')
+    except Exception as e:
+        print(f"Error reading file {file_key} from bucket {bucket_name}: {e}")
+        return None
+
+import boto3
+
+# ... [Code to define time filters, bucket names, and display the table] ...
+def compare_files(bucket_audio, bucket_text, start_time, end_time):
+    """Compare files in the audio and text buckets."""
+    audio_files = list_files(bucket_audio, '.flac', start_time, end_time)
+    text_files = list_files(bucket_text, '.txt', start_time, end_time)
+
+    # Extract base names without extensions
+    audio_base_names = {file.rsplit('.', 1)[0] for file in audio_files}
+    text_base_names = {file.rsplit('.', 1)[0] for file in text_files}
+
+    # Prepare the table
+    table = []
+    all_base_names = sorted(audio_base_names.union(text_base_names))
+
+    for base_name in all_base_names:
+        audio_file_name = f"{base_name}.flac"
+        text_file_name = f"{base_name}.txt"
+
+        # Determine if the audio file exists
+        if audio_file_name in audio_files:
+            audio_entry = audio_file_name
+        else:
+            audio_entry = f"No audio file ({audio_file_name})"
+
+        # Determine if the text file exists and get its content
+        if text_file_name in text_files:
+            text_content = get_file_content(bucket_text, text_file_name)
+            text_entry = f"{text_file_name}: {text_content}"
+        else:
+            text_entry = f"No transcription ({text_file_name})"
+
+        table.append((audio_entry, text_entry))
+
+    return table
+
+def generate_html_page(table, bucket_audio_url, output_file):
+    """Generate an HTML page from the table data."""
+    html_content = """
+    <html>
+    <head><title>Audio Transcriptions</title></head>
+    <body>
+        <table border='1'>
+            <tr>
+                <th>Audio File</th>
+                <th>Transcribed Text</th>
+            </tr>
+    """
+
+    for audio_entry, text_entry in table:
+        # Process audio file entry
+        if not audio_entry.startswith("No audio file"):
+            audio_url = f"{bucket_audio_url}/{audio_entry}"  # Full URL with https
+            audio_html = f"<audio controls src='{audio_url}'></audio><br>{audio_entry.rsplit('.', 1)[0]}"
+        else:
+            audio_html = audio_entry
+
+        # Remove the file reference from the transcription
+        text_content = text_entry.split(": ", 1)[1] if ": " in text_entry else text_entry
+
+        # Add row to HTML table
+        html_content += f"<tr><td>{audio_html}</td><td>{text_content}</td></tr>"
+
+    html_content += """
+        </table>
+    </body>
+    </html>
+    """
+
+    # Write to HTML file
+    with open(output_file, 'w') as file:
+        file.write(html_content)
+
+# Define your time filter, bucket names, and the full URL for the audio bucket
+start_time = '2023-12-09-17-00-00'
+end_time = '2023-12-09-59-15-00'
+bucket_audio_name = 'presigned-url-audio-uploads'
+bucket_text_name = 'audioclientserver-transcribedobjects-public'
+bucket_audio_url = 'https://presigned-url-audio-uploads.s3.us-east-2.amazonaws.com'
+
+# Generate the table and HTML page
+table = compare_files(bucket_audio_name, bucket_text_name, start_time, end_time)
+generate_html_page(table, bucket_audio_url, 'audio_transcriptions.html')
+
